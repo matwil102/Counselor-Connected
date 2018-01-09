7316e5a630675aa03b5942cde523c1fe842fcf68
Mellie Zito <melliezito@comcast.net>
2018-01-08 21:14:31 -0500

    Update and rename policies.html to policies.php

--- policies.php
+++ policies.php
@@ -28,7 +28,7 @@ background-color:#add8e6;
 <body>
 
 <h1>Counselor Connected</h1>
-<a href="main-page.html"><button><--Back</button></a>
+<a href="main-page.php"><button><--Back</button></a>
 
 <div id="policy">
 <a href="https://www.schoolcounselor.org/asca/media/asca/Ethics/EthicalStandards2016.pdf"><button>ASCA Guidelines<br><img src="ASCA.png"></button></a>
@@ -41,7 +41,7 @@ background-color:#add8e6;
   <li>Keep information confidential unless legal requirements demand otherwise.</li>
   <li>Report if the student intends to harm themselves or others.</li>
   <li>Convey highly sensitive information through phone call or private conversation.</li>
-  <li>Not be realted to the student immediately.</li>
+  <li>Not be related to the student immediately.</li>
   <li>Provide a list of resources for outside agencies for students in distress.</li>
   <li>Report incidents of bullyin, dating violence, child abuse, and sexual harassment.</li>
 </ul>
