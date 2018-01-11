77b84572f3ff0a2c24a8d927ff93de0952e667c4
Mellie Zito <melliezito@comcast.net>
2018-01-10 22:46:01 -0500

    add translator

--- profile.php
+++ profile.php
@@ -15,6 +15,9 @@ background-color:#add8e6;
      h2 {text-align:center;}
 
      #info {text-align: center;}
+    
+    #google_translate_element {text-align: right;}
+    
   </style>
 </head>
 
@@ -27,6 +30,18 @@ background-color:#add8e6;
 
 <h2>Profile</h2>
 
+<div id="google_translate_element">
+    <script type="text/javascript">
+      function googleTranslateElementInit() {
+  	new google.translate.TranslateElement({pageLanguage: 'en', includedLanguages: 'en,es,tl,vi,zh-CN',
+						layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 
+						'google_translate_element');
+      }
+    </script>
+    <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit">
+    </script>
+</div>
+  
 <div id="info">
   <img src="https://d30y9cdsu7xlg0.cloudfront.net/png/363633-200.png">
   <p>Name: Student Name</p>
